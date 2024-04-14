5b0827fd12ce4c8a2ffe911b9c66af779b9f2833
Abhishek Shankar <abhisheksdevadiga271@gmail.com>
2024-04-14 16:39:15 +0530

    new

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,43 +4,38 @@ import "./styles.css";
 
 const Accordion = () => {
   const [selected, setSelected] = useState(null);
-  const [enableMultiSelection,setEnableMultiSelection] =useState(false)
-  const [multipleSelected ,setMultipleSelected]=useState([])
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multipleSelected, setMultipleSelected] = useState([]);
 
   const handleSingleSelection = (getCurrentID) => {
-    
     setSelected(getCurrentID === selected ? null : getCurrentID);
   };
 
-
-  const handleMultiSelction=(getCurrentID)=>{
-    let cpyMultiple =[...multipleSelected];
-    const findIndexOfCurrentId =cpyMultiple.indexOf(getCurrentID)
-    if(findIndexOfCurrentId=== -1){
-        cpyMultiple.push(getCurrentID)
-    }else{
-        cpyMultiple.splice(findIndexOfCurrentId,1)
+  const handleMultiSelection = (getCurrentID) => {
+    let cpyMultiple = [...multipleSelected];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentID);
+    if (findIndexOfCurrentId === -1) {
+      cpyMultiple.push(getCurrentID);
+    } else {
+      cpyMultiple.splice(findIndexOfCurrentId, 1);
     }
-    setMultipleSelected(cpyMultiple)
-  }
+    setMultipleSelected(cpyMultiple);
+  };
 
   return (
     <div className="wrapper">
-        <button onClick={()=>setEnableMultiSelection(!enableMultiSelection)}>Enable multiple selection</button>
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        {enableMultiSelection ? "Disable multiple selection" : "Enable multiple selection"}
+      </button>
       <div className="accordion">
         {dummyData && dummyData.length > 0 ? (
           dummyData.map((data) => (
             <div className="item" key={data.id}>
-              <div className="title" onClick={enableMultiSelection ? ()=>handleMultiSelction(data.id) : () => handleSingleSelection(data.id)}>
+              <div className="title" onClick={enableMultiSelection ? () => handleMultiSelection(data.id) : () => handleSingleSelection(data.id)}>
                 <h3>{data.question}</h3>
-                <span>+</span>
+                {enableMultiSelection || selected !== data.id ? <span>+</span> : <span>-</span>}
               </div>
-              {
-                enableMultiSelection ? multipleSelected.indexOf(data.id) !== -1 && <h3 className="content">{data.answer} </h3> : selected ===data.id && (<h3>{data.answer}</h3>)
-              }
-              {/* {
-                selected===data.id || multipleSelected.indexOf(data.id !==-1)? <h3>{data.answer}</h3>:null
-              } */}
+              {enableMultiSelection ? (multipleSelected.indexOf(data.id) !== -1 && <h3 className="content">{data.answer}</h3>) : (selected === data.id && <h3>{data.answer}</h3>)}
             </div>
           ))
         ) : (
